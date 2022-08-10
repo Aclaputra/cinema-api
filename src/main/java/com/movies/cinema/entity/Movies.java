cca742dbf80c680396730e846358b3f82cbcaea7
Aclaputra <muhamadacla22@gmail.com>
2022-08-10 10:25:16 +0700

    feat: added one to many relationship from movies to cast

--- src/main/java/com/movies/cinema/entity/Movies.java
+++ src/main/java/com/movies/cinema/entity/Movies.java
@@ -1,13 +1,20 @@
 package com.movies.cinema.entity;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import com.movies.cinema.entity.Cast;
 
 @Data
 @AllArgsConstructor
@@ -22,4 +29,7 @@ public class Movies {
     private String language;
     private String status;
     
+    @OneToMany(targetEntity = Cast.class, cascade = CascadeType.ALL)
+    @JoinColumn(name = 'mc_fk', referencedColumn = 'id')
+    private List<Cast> casts;
 }
